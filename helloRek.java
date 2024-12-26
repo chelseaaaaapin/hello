99a010df66fdceda446bcb64e1a342b2b4040c78
chelseaaaaapin <chelseariyantashila06@gmail.com>
2024-12-26 07:56:32 +0700

    Add files via upload

---
+++ helloRek.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello;
+
+/**
+ *
+ * @author Chealsea
+ */
+public class helloRek {
+    public static void main(String[] args) {
+        System.out.println("Hallo REK");
+        System.out.println("HAPPY NEW YEAR REK");
+        System.out.println("INFO REK!!!");
+    }
+    
+}
